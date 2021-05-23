739e054172f1352d1d6254a640d02594d1935bf5
I-GALLA <igalla@outlook.fr>
2021-05-23 21:49:15 +0200

    add left-sidebar & top-navbar

--- backend/includes/footer.php
+++ backend/includes/footer.php
@@ -1,23 +1,24 @@
-  <!--start footer-->
-  <footer class="footer mt-auto footer-light">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-6 small">Copyright &#xA9; techno 2020</div>
-                            <div class="col-md-6 text-md-right small">
-                                <a href="#!">Privacy Policy</a>
-                                &#xB7;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
+                <!--start footer-->
+                <footer class="footer mt-auto footer-light">
+                 <div class="container-fluid">
+                  <div class="row">
+                   <div class="col-md-6 small">Copyright &#xA9; AMDI4 2021</div>
+                   <div class="col-md-6 text-md-right small">
+                    <a href="../privacy-policy.php" target="_blank">Mentions Légales</a>
+                    &#xB7;
+                    <a href="../CGU.php" target="_blank">CGU</a>
+                   </div>
+                  </div>
+                 </div>
                 </footer>
                 <!--end footer-->
-            </div>
-        </div>
+                </div>
+                </div>
 
-        <!--Script JS-->
-        <script src="js/jquery-3.4.1.min.js"></script>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/scripts.js"></script>
-    </body>
-</html>
+                <!--Script JS-->
+                <script src="js/jquery-3.4.1.min.js"></script>
+                <script src="js/bootstrap.bundle.min.js"></script>
+                <script src="js/scripts.js"></script>
+                </body>
+
+                </html>
