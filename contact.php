87e7448c017da6a90d9b731f5cef2bce911ca91a
I-GALLA <igalla@outlook.fr>
2021-05-23 22:39:48 +0200

    update contact  file

--- contact.php
+++ contact.php
@@ -1,12 +1,13 @@
-<?php $current_page = "Contact"; ?>
+<?php $current_page = "Contact us"; ?>
 <?php require_once("./includes/header.php"); ?>
+
 <div id="layoutDefault">
  <div id="layoutDefault_content">
   <main>
 
    <nav class="navbar navbar-marketing navbar-expand-lg bg-white navbar-light">
     <div class="container">
-     <a class="navbar-brand text-dark" href="index.php">techno</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><img src="./img/menu.png" style="height:20px;width:25px" /><i data-feather="menu"></i></button>
+     <a class="navbar-brand text-dark" href="index.php">AMDI BLOG</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><img src="./img/menu.png" style="height:20px;width:25px" /><i data-feather="menu"></i></button>
      <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto mr-lg-5">
        <li class="nav-item">
@@ -19,8 +20,10 @@
         <a class="nav-link" href="about.php">About</a>
        </li>
       </ul>
-      <a class="btn-teal btn rounded-pill px-4 ml-lg-4" href="backend/signin.php">Sign in<i class="fas fa-arrow-right ml-1"></i></a>
-      <a class="btn-teal btn rounded-pill px-4 ml-lg-4" href="backend/signup.php">Sign up<i class="fas fa-arrow-right ml-1"></i></a>
+      <?php
+      $curr_page = basename(__FILE__);
+      require_once("./includes/registration.php");
+      ?>
      </div>
     </div>
    </nav>
@@ -44,14 +47,92 @@
    <section class="bg-white py-10">
     <div class="container">
 
-     <form>
-      <div class="form-row">
-       <div class="form-group col-md-6"><label class="text-dark" for="inputName">Full name</label><input class="form-control py-4" id="inputName" type="text" placeholder="Full name" /></div>
-       <div class="form-group col-md-6"><label class="text-dark" for="inputEmail">Email</label><input class="form-control py-4" id="inputEmail" type="email" placeholder="name@example.com" /></div>
-      </div>
-      <div class="form-group"><label class="text-dark" for="inputMessage">Message</label><textarea class="form-control py-3" id="inputMessage" type="text" placeholder="Enter your message..." rows="4"></textarea></div>
-      <div class="text-center"><button class="btn btn-primary btn-marketing mt-4" type="submit">Submit Request</button></div>
-     </form>
+     <?php
+     if (isset($_COOKIE['_uid_']) || isset($_COOKIE['_uiid_']) || isset($_SESSION['login'])) { ?>
+      <form action="contact.php" method="POST">
+       <?php
+       if (isset($_COOKIE['_uid_'])) {
+        $user_id = base64_decode($_COOKIE['_uid_']);
+       } else if (isset($_SESSION['user_id'])) {
+        $user_id = $_SESSION['user_id'];
+       } else {
+        $user_id = -1;
+       }
+       $sql = "SELECT * FROM users WHERE user_id = :id";
+       $stmt = $pdo->prepare($sql);
+       $stmt->execute([
+        ':id' => $user_id
+       ]);
+       $user = $stmt->fetch(PDO::FETCH_ASSOC);
+       $user_name = $user['user_name'];
+       $user_email = $user['user_email'];
+
+       if (isset($_POST['send'])) {
+        $message = trim($_POST['message']);
+        $sql = "INSERT INTO messages SET ms_username = :username, ms_useremail = :email, ms_detail = :detail, ms_date = :date";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+         ':username' => $user_name,
+         ':email' => $user_email,
+         ':detail' => $message,
+         ':date' => date("M n, Y") . ' at ' . date("h:i A")
+        ]);
+        echo "<p class='alert alert-success'>Message has been send successfully!</p>";
+       }
+       ?>
+       <div class="form-row">
+        <div class="form-group col-md-6">
+         <label class="text-dark" for="inputName">Full name</label>
+         <input value="<?php echo $user_name; ?>" readonly="true" class="form-control py-4" id="inputName" type="text" placeholder="Full name" />
+        </div>
+        <div class="form-group col-md-6">
+         <label class="text-dark" for="inputEmail">Email</label>
+         <input value="<?php echo $user_email; ?>" readonly="true" class="form-control py-4" id="inputEmail" type="email" placeholder="name@example.com" />
+        </div>
+       </div>
+       <div class="form-group">
+        <label class="text-dark" for="inputMessage">Message</label>
+        <textarea name="message" class="form-control py-3" id="inputMessage" type="text" placeholder="Enter your message..." rows="4"></textarea>
+       </div>
+       <div class="text-center">
+        <button name="send" class="btn btn-primary btn-marketing mt-4" type="submit">Submit Request</button>
+       </div>
+      </form>
+
+
+
+      <table class="table table-bordered table-hover mt-5" id="dataTable" width="100%" cellspacing="0">
+       <thead>
+        <tr>
+         <th>Your messages:</th>
+         <th>Answers:</th>
+        </tr>
+       </thead>
+       <tbody>
+        <?php
+        $sql1 = "SELECT * FROM messages WHERE ms_useremail = :email";
+        $stmt1 = $pdo->prepare($sql1);
+        $stmt1->execute([
+         ':email' => $user_email
+        ]);
+        while ($ms = $stmt1->fetch(PDO::FETCH_ASSOC)) {
+         $ms_detail = $ms['ms_detail'];
+         $reply = $ms['reply']; ?>
+         <tr>
+          <td><?php echo $ms_detail; ?></td>
+          <td><?php echo $reply; ?></td>
+         </tr>
+        <?php }
+        ?>
+       </tbody>
+      </table>
+
+
+     <?php } else { ?>
+      <a href="./backend/sign-in.php">Sign in to contact us!</a>
+     <?php }
+     ?>
+
 
     </div>
 
@@ -69,7 +150,7 @@
     <div class="row align-items-center">
      <div class="col-md-6 small">Copyright &#xA9; AMDI4 2021</div>
      <div class="col-md-6 text-md-right small">
-      <a href="privacy-policy.php">Mentions légales</a>
+      <a href="privacy-policy.php">Mentions Légales</a>
       &#xB7;
       <a href="CGU.php">CGU</a>
      </div>
@@ -78,4 +159,5 @@
   </footer>
  </div>
 </div>
+
 <?php require_once("./includes/footer.php"); ?>
